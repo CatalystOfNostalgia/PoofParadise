d50b4df71ed05a93332511f9479a348788400ee2
Anthony Dario <anthonyrdario@gmail.com>
2015-09-14 16:00:38 -0400

    Added create statements for the friends table
    The friends table just has two columns linking the two friends
    together.

--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -2,34 +2,42 @@ CREATE SCHEMA IF NOT EXISTS gravehub
 USE gravehub;
 
 CREATE TABLE user(
-    user_id long,
+	user_id long,
 	name varchar(100),
 	email varchar(100),
-    username varchar(100),
-    last_login date,
+	username varchar(100),
+	last_login date,
 	password varchar(100),
-    level long,
-    PRIMARY KEY (userID)
+	level long,
+	PRIMARY KEY (userID)
 );
 
 CREATE TABLE user_building(
-    user_id long,
-    building_type ENUM  ('cave', 'windmill', 'campfire', 'pond', 'townhall'),
+	user_id long,
+	building_type ENUM  ('cave', 'windmill', 'campfire', 'pond', 'townhall'),
 	level long,
 	points long,
-    resource_production_rate long,
-    resource_type ENUM ('fire', 'earth', 'air', 'water'),
-    capacity long
+	resource_production_rate long,
+	resource_type ENUM ('fire', 'earth', 'air', 'water'),
+	capacity long
 );
 
 CREATE TABLE `building_info`(
-    `id_building_info` long NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NULL,
-    `structure_type` INT NOT NULL,
-    `resource_cost_fire` long NULL,
-    `resource_cost_water` long NULL,
-    `resource_cost_earth` long NULL,
-    `resource_cost_air` long NULL,
-    `resource_gather_rate` long  NULL,
-    PRIMARY KEY (`id_building_info`)
+	`id_building_info` long NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NULL,
+	`structure_type` INT NOT NULL,
+	`resource_cost_fire` long NULL,
+	`resource_cost_water` long NULL,
+	`resource_cost_earth` long NULL,
+	`resource_cost_air` long NULL,
+	`resource_gather_rate` long  NULL,
+	PRIMARY KEY (`id_building_info`)
+);
+
+CREATE TABLE friends(
+	friend1_id long, 
+	friend2_id long,
+	PRIMARY KEY (friend1_id, friend2_id),
+	FOREIGN KEY (friend1_id) REFERENCES user(user_id),
+	FOREIGN kEY (friend2_id) REFERENCES user(user_id)
 );
