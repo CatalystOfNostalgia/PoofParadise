bf39d7ea38730f8180e6517316a5b214d3e83f1b
Eric Luan <eluan@YELP-ELUAN.local>
2015-09-22 00:17:35 -0400

    Created new tables and updated sqlachemy with new values

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -7,5 +7,7 @@ db.echo = False
 Session = sessionmaker(bind=db)
 session = Session()
 
-#Import all our models here. Will make doing table operations much easier
+# Import all our models here. Will make doing table operations much easier. Please import in alphabetical order.
 from user import *
+from user_building import *
+
