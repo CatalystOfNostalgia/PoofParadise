ca2bc21be02ef8e4c1ea163dc9961fd0b3fa3aee
tkk21 <tkk21@case.edu>
2015-09-24 09:37:15 -0400

    added more sample sql queries

--- server/samplesqlcommands.py
+++ server/samplesqlcommands.py
@@ -1,13 +1,32 @@
 import models
 
 def sample_insert():
-    user = models.user.User(name='ted', email='asdf@1234.com', username = 'abc')
-    models.session.add(user)
-    models.session.commit()
+    if not exists('ted'):
+        user = models.user.User(name='ted', email='ted@case.edu', username = 'ted1')
+        models.session.add(user)
+        print models.session.commit()
+        "ted is added"
+    if not exists ('eric'):
+        user = models.user.User(name='eric', email='eric@case.edu', username = 'eric1')
+        models.session.add(user)
+        models.session.commit()
+        print  "eric is added"
 
 def sample_select():
-    ted = models.session.query(models.User).filter(models.User.username=='abc').first()
+    ted = models.session.query(models.User).filter(models.User.username=='ted1').one()
     print ted.email
-
+    eric = models.session.query(models.User).filter(models.User.username=='eric1').one()
+    print eric.email
+def sample_remove():
+    ted = models.session.query(models.User).filter(models.User.name=='ted').one()
+    models.session.delete(ted)
+    models.session.commit()
+    print "ted is deleted"
+def exists(name):
+    if models.session.query(models.User).filter(models.User.name == name).count() == 0:
+        return False
+    else:
+        return True
+sample_remove()
 sample_insert()
 sample_select()
