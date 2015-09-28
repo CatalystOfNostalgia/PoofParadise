de7a2ee139c5c0d4ce69c2907334512e809d76e1
tkk21 <tkk21@case.edu>
2015-09-28 00:32:08 -0400

    now adds random building to users and print them

--- server/samplesqlcommands.py
+++ server/samplesqlcommands.py
@@ -3,44 +3,63 @@ import random
 
 name_pool = ['Ted', 'Margaret', 'Eric', 'Anthony', 'Jeremy', 'Anjana', 'Robert', 'Alex', 'Austin', 'Brittany', 'William', 'Bryn', 'Zach', 'Alberto', 'Ian', 'Dan', 'Madison', 'Dylan', 'Lisa']
 
-def sample_insert_users():
+def sample_insert_group():
     for name in name_pool:
-        user = models.user.User(name=name, email=name+'@gravehub.com', username = name+'1', password = 'password')
-        models.session.add(user)
-        models.session.commit()
-        print name + " is added"
-    if not exists('ted'):
-        user = models.user.User(name='ted', email='ted@case.edu', username = 'ted1')
-        models.session.add(user)
-        models.session.commit()
-        print "ted is added"
-    if not exists ('eric'):
-        user = models.user.User(name='eric', email='eric@case.edu', username = 'eric1')
-        models.session.add(user)
-        models.session.commit()
-        print  "eric is added"
+        if not exists(name):
+            sample_insert(name)
 
-def sample_insert_building(user_id):
-    level = random.randrange(10)
-    build1 = models.user_building.UserDecorativeBuilding(user_id=user_id, building_info_id=0, level=1)
+def sample_select_group():
+    for name in name_pool:
+        if exists(name):
+            sample_select_email(name)
+def sample_insert_building_group():
+    for name in name_pool:
+        if (exists(name)):
+            for i in range(5):
+                sample_insert_building(name)
+
+def sample_select_buildings_group():
+    for name in name_pool:
+        if (exists(name)):
+            sample_select_buildings(name)
+
+def sample_insert_building(name):
+    user_id = models.session.query(models.User).filter(models.User.name==name).one().user_id
+    lv = random.randrange(10)
+    building_info_id = random.randrange(20)
+    build1 = models.user_building.UserDecorativeBuilding(user_id=user_id, building_info_id=building_info_id, level=lv, position_x=0, position_y=0)
     models.session.add(build1)
     models.session.commit()
+    print (name + " added a building")
 
-def sample_select():
-    ted = models.session.query(models.User).filter(models.User.username=='ted1').one()
-    print ted.email
-    eric = models.session.query(models.User).filter(models.User.username=='eric1').one()
-    print eric.email
-def sample_remove():
-    ted = models.session.query(models.User).filter(models.User.name=='ted').one()
-    models.session.delete(ted)
+def sample_insert(name):
+    user = models.user.User(name=name, email=name+'@gravehub.com', username = name+'1', password = 'password')
+    models.session.add(user)
     models.session.commit()
-    print "ted is deleted"
+    print (name + " is added")
+
+def sample_select_email(name):
+    selected  = models.session.query(models.User).filter(models.User.name==name).one()
+    print (name + "'s email is: " + selected.email)
+
+def sample_select_buildings(name):
+    user_id = models.session.query(models.User).filter(models.User.name==name).one().user_id
+    selected = models.session.query(models.UserDecorativeBuilding).filter(models.UserDecorativeBuilding.user_id==user_id).all()
+    for building in selected:
+        print (name + ' has a building with info_id ' + str(building.building_info_id) + ' and with level: ' + str(building.level))
+    
+def sample_remove(name):
+    selected = models.session.query(models.User).filter(models.User.name==name).one()
+    models.session.delete(selected)
+    models.session.commit()
+    print (name + " is deleted")
+
 def exists(name):
     if models.session.query(models.User).filter(models.User.name == name).count() == 0:
         return False
     else:
         return True
-sample_remove()
-sample_insert_users()
-sample_select()
+sample_insert_group()
+sample_select_group()
+sample_insert_building_group()
+sample_select_buildings_group()
