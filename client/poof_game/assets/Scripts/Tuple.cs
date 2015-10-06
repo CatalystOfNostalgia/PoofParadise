81ff87e1ebcca07b69f8b2334ffb348c098377c7
jrg94 <jrg170@case.edu>
2015-10-06 16:31:15 -0400

    Edited object hierarchy of game grid (original still in comments)

--- client/poof_game/assets/Scripts/Tuple.cs
+++ client/poof_game/assets/Scripts/Tuple.cs
@@ -14,4 +14,25 @@ public class Tuple {
         this.x = x;
         this.y = y;
 	}
+
+    public override bool Equals(System.Object o)
+    {
+        if (o == null)
+        {
+            return false;
+        }
+
+        Tuple test = o as Tuple;
+        if ((System.Object)o == null)
+        {
+            return false;
+        }
+
+        return test.x == x && test.y == y;
+    }
+
+    public override int GetHashCode()
+    {
+        return x ^ y;
+    }
 }
