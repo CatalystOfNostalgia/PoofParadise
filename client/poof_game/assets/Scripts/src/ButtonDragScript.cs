41e37065d655cf20353d4d4eb81a5c90b0108684
Ted Kim <tkk21@case.edu>
2015-10-10 20:19:17 -0400

    made a script that make button draggable

---
+++ client/poof_game/assets/Scripts/src/ButtonDragScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonDragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public bool dragOnSurfaces = true;
+	private GameObject draggingIcon;
+	private RectTransform draggingPlane;
+
+	public void OnBeginDrag (PointerEventData eventData){
+		Canvas canvas = GetComponent<Canvas>(gameObject);
+		if (canvas == null) {
+			Debug.Log ("ButtonDragScript: could not find canvas");
+			return;
+		}
+		
+		// We have clicked something that can be dragged.
+		// What we want to do is create an icon for this.
+		draggingIcon = new GameObject("icon");
+		
+		draggingIcon.transform.SetParent (canvas.transform, false);
+		draggingIcon.transform.SetAsLastSibling();
+		
+		var image = draggingIcon.AddComponent<Image>();
+		// The icon will be under the cursor.
+		// We want it to be ignored by the event system.
+		//draggingIcon.AddComponent<IgnoreRaycast>();
+		
+		image.sprite = GetComponent<Image>().sprite;
+		image.SetNativeSize();
+		
+		if (dragOnSurfaces)
+			draggingPlane = transform as RectTransform;
+		else
+			draggingPlane = canvas.transform as RectTransform;
+		
+		setDraggedPosition(eventData);
+	}
+
+	public void onDrag (PointerEventData eventData){
+		if (draggingIcon != null) {
+			setDraggedPosition(eventData);
+		}
+	}
+
+	private void setDraggedPosition (PointerEventData eventData){
+		if (dragOnSurfaces && eventData.pointerEnter != null && eventData.pointerEnter.transform as RectTransform != null)
+			draggingPlane = eventData.pointerEnter.transform as RectTransform;
+		
+		var rt = draggingIcon.GetComponent<RectTransform>();
+		Vector3 globalMousePos;
+		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingPlane, out globalMousePos, eventData.position, eventData.pressEventCamera))
+		{
+			rt.position = globalMousePos;
+			rt.rotation = draggingPlane.rotation;
+		}
+	}
+
+	public void endDrag (PointerEventData eventData){
+		if (draggingIcon != null) {
+			Destroy(draggingIcon);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
