e152ee464cfd5d655a6050ee45c12e53c86387e4
Ted Kim <tkk21@case.edu>
2015-10-10 20:42:23 -0400

    got button to follow cursor, but it's huge

--- client/poof_game/assets/Scripts/src/ButtonDragScript.cs
+++ client/poof_game/assets/Scripts/src/ButtonDragScript.cs
@@ -10,7 +10,8 @@ public class ButtonDragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, 
 	private RectTransform draggingPlane;
 
 	public void OnBeginDrag (PointerEventData eventData){
-		Canvas canvas = GetComponent<Canvas>(gameObject);
+		//Canvas canvas = GetComponent<Canvas>();
+		Canvas canvas = FindInParents<Canvas> (gameObject);
 		if (canvas == null) {
 			Debug.Log ("ButtonDragScript: could not find canvas");
 			return;
@@ -29,7 +30,10 @@ public class ButtonDragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, 
 		//draggingIcon.AddComponent<IgnoreRaycast>();
 		
 		image.sprite = GetComponent<Image>().sprite;
-		image.SetNativeSize();
+
+		RectTransform rekt = GetComponent<RectTransform> ();
+		image.SetNativeSize ();
+		//image.rectTransform.sizeDelta = rekt.sizeDelta;
 		
 		if (dragOnSurfaces)
 			draggingPlane = transform as RectTransform;
@@ -39,7 +43,7 @@ public class ButtonDragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, 
 		setDraggedPosition(eventData);
 	}
 
-	public void onDrag (PointerEventData eventData){
+	public void OnDrag (PointerEventData eventData){
 		if (draggingIcon != null) {
 			setDraggedPosition(eventData);
 		}
@@ -50,20 +54,37 @@ public class ButtonDragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, 
 			draggingPlane = eventData.pointerEnter.transform as RectTransform;
 		
 		var rt = draggingIcon.GetComponent<RectTransform>();
-		Vector3 globalMousePos;
-		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingPlane, out globalMousePos, eventData.position, eventData.pressEventCamera))
-		{
-			rt.position = globalMousePos;
-			rt.rotation = draggingPlane.rotation;
-		}
+		rt.position = Input.mousePosition;
+//		Vector3 globalMousePos;
+//		if (RectTransformUtility.PointInRectangleGlobalSpace(draggingPlane, out globalMousePos, eventData.position, eventData.pressEventCamera))
+//		{
+//			rt.position = globalMousePos;
+//			rt.rotation = draggingPlane.rotation;
+//		}
 	}
 
-	public void endDrag (PointerEventData eventData){
+	public void OnEndDrag (PointerEventData eventData){
 		if (draggingIcon != null) {
 			Destroy(draggingIcon);
 		}
 	}
 
+	static public T FindInParents<T>(GameObject go) where T : Component
+	{
+		if (go == null) return null;
+		var comp = go.GetComponent<T>();
+		
+		if (comp != null)
+			return comp;
+		
+		Transform t = go.transform.parent;
+		while (t != null && comp == null)
+		{
+			comp = t.gameObject.GetComponent<T>();
+			t = t.parent;
+		}
+		return comp;
+	}
 	// Update is called once per frame
 	void Update () {
 	
