017795e3751e20c1c5d236aa9c67eead524337ad
Ted Kim <tkk21@case.edu>
2015-10-16 23:15:46 -0400

    changed the implementation of sound manager

--- client/poof_game/assets/Scripts/src/Managers/SoundManager.cs
+++ client/poof_game/assets/Scripts/src/Managers/SoundManager.cs
@@ -3,21 +3,56 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 	
-	AudioSource[] child;
+	AudioSource[] playlist { get; set;}
+	AudioSource currentSong {get;}
+	bool currentSongPlayed;
+	float musicVolume { get; set; }
+
 	int index;
-	
+	private static SoundManager soundManager;
+
+	public static SoundManager Instance(){
+		if (!soundManager) {
+			soundManager = FindObjectOfType(typeof (SoundManager)) as SoundManager;
+			if(!soundManager)
+				Debug.LogError ("There needs to be one active SoundManager script on a GameObject in your scene.");
+		}
+		return soundManager;
+	}
+
+	public AudioSource[] getAvailableMusic(){
+		return this.GetComponentsInChildren<AudioSource> ();
+	}
+
+	public void stopSong(){
+
+	}
+
 	// Use this for initialization
 	void Start () {
 		index = 0;
-		child = this.GetComponentsInChildren<AudioSource>();
+
+		//in the future, load user's music preference
+		playlist = getAvailableMusic();
+		musicVolume = 100f;
+
+		currentSong = playlist [index];
+		currentSongPlayed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (!child[index % child.Length].isPlaying && !child[(index + 1) % child.Length].isPlaying) {
-			child[(index + 1) % child.Length].Play();
-			index++;
+		if (!currentSong.isPlaying) {
+			//didn't start the song yet
+			if (!currentSongPlayed){
+				currentSong.Play();
+				currentSongPlayed = true;
+			}
+			else {//finished the song
+				currentSongPlayed = false;
+				index++;
+				currentSong = playlist[index % playlist.Length];
+			}
 		}
 	}
 }
