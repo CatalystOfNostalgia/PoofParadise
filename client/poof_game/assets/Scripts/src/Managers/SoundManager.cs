28d7f5199f8980a9f5971b8a984aa307960c0633
jrg94 <jrg170@case.edu>
2015-11-05 15:54:17 -0500

    Improving readability

--- client/poof_game/assets/Scripts/src/Managers/SoundManager.cs
+++ client/poof_game/assets/Scripts/src/Managers/SoundManager.cs
@@ -27,10 +27,62 @@ public class SoundManager : Manager {
 
 	int index;
 
-	public AudioSource[] getAvailableMusic(){
+    /**
+     * Initializes SoundManager as a singleton
+     *
+     * Initializes song dictionary
+     */
+    override public void Start()
+    {
+
+        // Converts SoundManager into a singleton
+        if (soundManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            soundManager = this;
+        }
+        else if (soundManager != this)
+        {
+            Destroy(gameObject);
+        }
+
+        index = 0;
+
+        //in the future, load user's music preference
+        playlist = getAvailableMusic();
+        preferredPlaylist = new bool[playlist.Length];
+        for (int i = 0; i < preferredPlaylist.Length; i++)
+        {
+            preferredPlaylist[i] = true;
+        }
+        playDict = new Dictionary<string, AudioSource>();
+        foreach (AudioSource music in playlist)
+        {
+            playDict.Add(music.name, music);
+            Debug.Log("SoundManager: Added " + music.name + " to the dictionary");
+        }
+        masterVolume = 1f;
+        musicVolume = 1f;
+        soundVolume = 1f;
+
+        currentSong = playlist[index];
+        currentSongPlayed = false;
+        allSongsDisabled = false;
+    }
+
+    /**
+     * Generates the song list from the songs in children
+     *
+     * TODO: Pull songs from directory and generate tracklist
+     */
+    public AudioSource[] getAvailableMusic(){
 		return this.GetComponentsInChildren<AudioSource> ();
 	}
 
+    /**
+     * A function which takes a song name as 
+     * input and plays that song
+     */
 	public void playSong(string songName){
 		AudioSource song;
 		if (playDict.TryGetValue (songName, out song) && !changingSong) {
@@ -46,6 +98,10 @@ public class SoundManager : Manager {
 		}
 	}
 
+    /**
+     * A function which stops that song that
+     * is currently playing
+     */
 	public void stopSong(){
 		if (currentSong == null) {
 			return;
@@ -53,6 +109,10 @@ public class SoundManager : Manager {
 		currentSong.Stop ();
 	}
 
+    /**
+     * A function which plays the next song
+     * in the playlist
+     */
 	public void nextSong(){
 		stopSong (); //might be redundant
 		for (int i = 0; i<playlist.Length; i++) {
@@ -68,6 +128,10 @@ public class SoundManager : Manager {
 		Debug.Log ("SoundManager: All music are disabled");
 	}
 
+    /**
+     * A function which plays the previous song
+     * in the playlist
+     */
 	public void previousSong(){
 		stopSong (); //might be redundant
 		for (int i = 0; i<playlist.Length; i++) {
@@ -83,6 +147,10 @@ public class SoundManager : Manager {
 		Debug.Log ("SoundManager: All music are disabled");
 	}
 
+    /**
+     * Not sure what this does
+     * TODO: Write comment
+     */
 	public void setPreferredPlaylist (string song){
 		//maybe I could have referenced the toggle to here and avoid the search
 		int i = findIndex (song);
@@ -95,6 +163,9 @@ public class SoundManager : Manager {
 		preferredPlaylist [i] = !preferredPlaylist [i];
 	}
 
+    /**
+     * Generates the index for a song name
+     */
 	private int findIndex (string songName){
 		for (int i = 0; i<playlist.Length; i++){
 			if (playlist[i].name.Equals(songName)){
@@ -103,44 +174,10 @@ public class SoundManager : Manager {
 		}
 		return -1;
 	}
-
-	// Use this for initialization
-	override public void Start () {
-
-        // Converts SoundManager into a singleton
-        if (soundManager == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            soundManager = this;
-        }
-        else if (soundManager != this)
-        {
-            Destroy(gameObject);
-        }
-
-        index = 0;
-
-		//in the future, load user's music preference
-		playlist = getAvailableMusic();
-		preferredPlaylist = new bool[playlist.Length];
-		for (int i = 0; i<preferredPlaylist.Length; i++) {
-			preferredPlaylist[i] = true;
-		}
-		playDict = new Dictionary<string, AudioSource> ();
-		foreach (AudioSource music in playlist) {
-			playDict.Add(music.name, music);
-			Debug.Log("SoundManager: Added " + music.name + " to the dictionary");
-		}
-		masterVolume = 1f;
-		musicVolume = 1f;
-		soundVolume = 1f;
-
-		currentSong = playlist [index];
-		currentSongPlayed = false;
-		allSongsDisabled = false;
-	}
 	
-	// Update is called once per frame
+	/**
+     * Update is called once per frame
+     */
 	void Update () {
 		if (allSongsDisabled) {
 			currentSong = null;
