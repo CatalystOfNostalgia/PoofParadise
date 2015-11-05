02aa0b8f4ae7dc0554b14943b1d197c636f44872
jrg94 <jrg170@case.edu>
2015-11-05 14:01:52 -0500

    UNHACKATHON - Part 5
    Sound panel is now fully incorporated in game start

--- client/poof_game/assets/Scripts/src/UI/SettingsMenu.cs
+++ client/poof_game/assets/Scripts/src/UI/SettingsMenu.cs
@@ -15,6 +15,11 @@ public class SettingsMenu : MonoBehaviour {
 	public Button playButtonEarth;
 	public Button playButtonFire;
 	public Button playButtonWater;
+    public Button nextSong;
+
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider soundVolumeSlider;
 
 	public Button exit;
 
@@ -25,28 +30,43 @@ public class SettingsMenu : MonoBehaviour {
 	void Start () {
 	}
 
+    /**
+     * Adds functionality to all of the buttons on the panel
+     */
 	public void generatePanel(){
 		this.gameObject.SetActive (true);
 		AudioSource[] music = SoundManager.soundManager.playlist;
-		//idk why loop approach doesn't work. it only plays water theme
-//		foreach (Button button in playButtons) {
-//			button.onClick.RemoveAllListeners();
-//			button.onClick.AddListener (() => soundManager.playSong(button.name));
-//			Debug.Log("SettingsMenu: added " + button.name + "'s action listener");
-//		}
+
 		playButtonAir.onClick.RemoveAllListeners ();
 		playButtonAir.onClick.AddListener (() => SoundManager.soundManager.playSong(playButtonAir.name));
+
 		playButtonEarth.onClick.RemoveAllListeners ();
 		playButtonEarth.onClick.AddListener (() => SoundManager.soundManager.playSong(playButtonEarth.name));
+
 		playButtonFire.onClick.RemoveAllListeners ();
 		playButtonFire.onClick.AddListener (() => SoundManager.soundManager.playSong(playButtonFire.name));
+
 		playButtonWater.onClick.RemoveAllListeners ();
 		playButtonWater.onClick.AddListener (() => SoundManager.soundManager.playSong(playButtonWater.name));
+
 		exit.onClick.RemoveAllListeners ();
 		exit.onClick.AddListener (ClosePanel);
-	}
 
-	void ClosePanel(){
+        nextSong.onClick.RemoveAllListeners();
+        nextSong.onClick.AddListener(() => SoundManager.soundManager.nextSong());
+
+        // It would probably be easier to just write a function for the slider, but...
+        masterVolumeSlider.onValueChanged.RemoveAllListeners();
+        masterVolumeSlider.onValueChanged.AddListener(delegate { SoundManager.soundManager.masterVolume = masterVolumeSlider.value; });
+
+        musicVolumeSlider.onValueChanged.RemoveAllListeners();
+        musicVolumeSlider.onValueChanged.AddListener(delegate { SoundManager.soundManager.musicVolume = musicVolumeSlider.value; });
+
+        soundVolumeSlider.onValueChanged.RemoveAllListeners();
+        soundVolumeSlider.onValueChanged.AddListener(delegate { SoundManager.soundManager.soundVolume = soundVolumeSlider.value; });
+    }
+
+    void ClosePanel(){
 		this.gameObject.SetActive(false);
 	}
 }
