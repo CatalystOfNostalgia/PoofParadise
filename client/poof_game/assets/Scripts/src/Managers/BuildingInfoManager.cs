e2a561e558b853c2932f8c4c86684fee6a2a9d2c
Ted Kim <tkk21@case.edu>
2015-11-08 12:46:02 -0500

    implemented building info manager
    hard coded info for now, option to import from json later

---
+++ client/poof_game/assets/Scripts/src/Managers/BuildingInfoManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildingInfoManager : MonoBehaviour {
+
+	private Dictionary<string, BuildingInfo> infoDict;
+
+	// Use this for initialization
+	void Start () {
+		infoDict = new Dictionary<string, BuildingInfo> ();
+	}
+
+	public BuildingInfo getInfo(string key){
+		BuildingInfo value;
+		if (infoDict.TryGetValue(key, out value)){
+			return value;
+		}
+		return null;
+	}
+
+	public void populateDictFromJSON(){
+
+	}
+
+	private void sampleAddInfo(){
+
+	}
+}
---
+++ client/poof_game/assets/Scripts/src/UI/BuildingInfo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingInfo : MonoBehaviour {
+	private string name;
+	private string description;
+	private int fireCost;
+	private int waterCost;
+	private int airCost;
+	private int earthCost;
+
+	public BuildingInfo(string name, string description, int fireCost, int waterCost, int airCost, int earthCost){
+		this.name = name;
+		this.description = description;
+		this.fireCost = fireCost;
+		this.waterCost = waterCost;
+		this.airCost = airCost;
+		this.earthCost = earthCost;
+	}
+
+	public string Name { get; }
+	public string Description { get; }
+	public int FireCost { get; }
+	public int WaterCost { get; }
+	public int AirCost { get; }
+	public int EarthCost { get; }
+}
