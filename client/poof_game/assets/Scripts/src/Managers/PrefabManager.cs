bb42dd077823f76ee87587788a5e77c246db2630
jrg94 <jrg170@case.edu>
2015-11-10 18:52:25 -0500

    Removed Buttons prefabs from Resources
    Decided to do away with this folder. All UI elements will be generated
    from Panels. Buttons can be pulled from an external directory.

--- client/poof_game/assets/Scripts/src/Managers/PrefabManager.cs
+++ client/poof_game/assets/Scripts/src/Managers/PrefabManager.cs
@@ -12,7 +12,6 @@ public class PrefabManager : Manager {
     public DecorativeBuilding[] decorativeBuildigs { get; set; }
     public Tile[] tiles { get; set; }
     public NPC[] npcs { get; set; }
-    public Button[] buttons { get; set; }
     public CanvasRenderer[] panels { get; set; }
     public Canvas canvas { get; set; }
 
@@ -42,7 +41,6 @@ public class PrefabManager : Manager {
         decorativeBuildigs = Resources.LoadAll("Prefabs/Buildings/Decorative Buildings", typeof(DecorativeBuilding)).Cast<DecorativeBuilding>().ToArray();
         tiles = Resources.LoadAll("Prefabs/Grid", typeof(Tile)).Cast<Tile>().ToArray();
         npcs = Resources.LoadAll("Prefabs/NPCs", typeof(NPC)).Cast<NPC>().ToArray();
-        buttons = Resources.LoadAll("Prefabs/UI/Buttons", typeof(Button)).Cast<Button>().ToArray();
         canvas = (Canvas)Resources.Load("Prefabs/UI/Canvas", typeof(Canvas));
         panels = Resources.LoadAll("Prefabs/UI/Panels", typeof(CanvasRenderer)).Cast<CanvasRenderer>().ToArray();
     }
