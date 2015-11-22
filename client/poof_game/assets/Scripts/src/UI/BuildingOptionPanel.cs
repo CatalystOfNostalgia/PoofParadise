4d568dafbaf355f730fb622515a28aad2a861613
Ted Kim <tkk21@case.edu>
2015-11-22 17:17:42 -0500

    implemented building option panel skeleton

---
+++ client/poof_game/assets/Scripts/src/UI/BuildingOptionPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingOptionPanel : GamePanel {
+
+	//move, upgrade, remove, info
+	private Button[] buttons;
+
+	//lets make the menu set inactive when you press outside
+
+	// Use this for initialization
+	override public void Start () {
+		buttons = RetrieveButtonList ("Buttons");
+		GeneratePanel ();
+	}
+
+	override public void GeneratePanel(){
+		FindAndModifyUIElement("Move Button", buttons, ()=> ); 
+		FindAndModifyUIElement("Upgrade Button", buttons, ()=> ); 
+		FindAndModifyUIElement("Remove Button", buttons, ()=> ); 
+		FindAndModifyUIElement("Info Button", buttons, ()=> ); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
