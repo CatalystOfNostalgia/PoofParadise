c25909820f8981c6401517f588acf0c1e636615d
jrg94 <jrg170@case.edu>
2015-12-06 16:51:56 -0500

    Working on adding buildings to building menu

--- client/poof_game/assets/Scripts/src/UI/BuildingPanel.cs
+++ client/poof_game/assets/Scripts/src/UI/BuildingPanel.cs
@@ -15,7 +15,11 @@ public class BuildingPanel : GamePanel {
 	private Button[] resourceButtons;
     private Button[] decorativeButtons;
 
-    private int activePanel;
+    private int resourceIndex;
+    private int decorativeIndex;
+
+    private enum panel : int { DECORATIVE, RESOURCE };
+    private panel activePanel; 
 
     public Button prefab;
 	
@@ -33,8 +37,10 @@ public class BuildingPanel : GamePanel {
 		else if (buildingPanel != this) {
 			Destroy(gameObject);
 		}
-        activePanel = 1;
-        this.transform.GetChild(activePanel).gameObject.SetActive(true);
+        decorativeIndex = 0;
+        resourceIndex = 0;
+        activePanel = panel.RESOURCE;
+        this.transform.GetChild((int)activePanel).gameObject.SetActive(true);
         resourceButtons = CreateButtons("Resource Building Panel/Buttons", PrefabManager.prefabManager.resourceBuildings);
         decorativeButtons = CreateButtons("Decorative Building Panel/Buttons", PrefabManager.prefabManager.decorativeBuildigs);
 		GeneratePanel();
@@ -55,6 +61,25 @@ public class BuildingPanel : GamePanel {
             b.onClick.AddListener(TogglePanel);
         }
 	}
+
+    /**
+     * Generates buttons per panel
+     */
+    public void AddButtonsToPanel(Button[] buttons)
+    {
+        // If we are adding buttons to the decorative building panel
+        if (activePanel == panel.DECORATIVE) {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                // TODO: place buttons under the building info assets
+            }
+        }
+        // If we are adding buttons to the resource building panel
+        else
+        {
+            // Repeat code above -> or just make a method you heathen
+        }
+    }
 	
     /**
      * Dynamically creates buttons
@@ -62,10 +87,17 @@ public class BuildingPanel : GamePanel {
      */
 	public Button[] CreateButtons(string path, Building[] buildingList)
     {
-        List<Button> list = new List<Button>();
-        int i = 0;
-        foreach (Building b in buildingList)
+        if (buildingList.Length > 4)
         {
+            Debug.LogError("Building lists must be size 4");
+            return null;
+        }
+        else
+        {
+            List<Button> list = new List<Button>();
+            int i = 0;
+            foreach (Building b in buildingList)
+            {
                 SpriteRenderer sr = b.GetComponent<SpriteRenderer>();
                 Button button = (Button)Instantiate(prefab);
                 button.transform.SetParent(this.transform.Find(path));
@@ -74,10 +106,11 @@ public class BuildingPanel : GamePanel {
                 button.name = b.name;
                 button.GetComponentInChildren<Text>().text = b.name;
                 button.GetComponent<RectTransform>().sizeDelta = new Vector2(140, 120);// Set(i * 100 + 50, 50, 140, 120);
-                button.transform.position = new Vector3(i * 100 + this.transform.position.x - (buildingList.Length*100/2), 100);
+                button.transform.position = new Vector3(i * 100 + this.transform.position.x - (buildingList.Length * 100 / 2), 100);
                 button.gameObject.AddComponent<ButtonDragScript>().ID = b.ID;
                 i++;
+            }
+            return list.ToArray();
         }
-        return list.ToArray();
     }
 }
