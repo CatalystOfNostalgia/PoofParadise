712ee045852e45dba576b281693f153ecda0804f
jrg94 <jrg170@case.edu>
2015-12-06 00:41:25 -0500

    Basic drag feature has been implemented
    This feature will need to be modified. It currently only works once, and
    it kills the collider. Also, I imagine this messes with the dictionary
    as the area where the building used to be is still red.

--- client/poof_game/assets/Scripts/src/UI/BuildingOptionPanel.cs
+++ client/poof_game/assets/Scripts/src/UI/BuildingOptionPanel.cs
@@ -6,24 +6,39 @@ public class BuildingOptionPanel : GamePanel {
 
 	//move, upgrade, remove, info
 	private Button[] buttons;
+    private Building building;
 
 	//lets make the menu set inactive when you press outside
 
 	// Use this for initialization
 	override public void Start () {
 		buttons = RetrieveButtonList ("Buttons");
+        SetBuilding();
 		GeneratePanel ();
 	}
 
 	override public void GeneratePanel(){
-		FindAndModifyUIElement("Move Button", buttons, ()=> Debug.Log("Move button is pressed"));
+		FindAndModifyUIElement("Move Button", buttons, ()=> Move());
 		FindAndModifyUIElement("Upgrade Button", buttons, ()=> Debug.Log("Upgrade button is pressed"));
 		FindAndModifyUIElement("Remove Button", buttons, ()=> Debug.Log("Remove button is pressed"));
 		FindAndModifyUIElement("Info Button", buttons, ()=> Debug.Log("Info button is pressed"));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    /**
+     * Establishes the link between this panel and its building
+     */
+    private void SetBuilding()
+    {
+        building = this.transform.GetComponentInParent<Building>();
+    }
+
+    /**
+     * Provides the move functionality for buildings
+     */
+    private void Move()
+    {
+        building.canDrag = true;
+        this.gameObject.SetActive(false);
+        //TogglePanel();
+    }
 }
