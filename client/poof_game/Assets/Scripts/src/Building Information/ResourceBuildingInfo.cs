3ad56a6ed97990c6de315f2cc84028ea9df9f5fc
Ted <tkk21@case.edu>
2015-12-06 22:18:48 -0500

    resource building info implemented
    using the building information class as base

---
+++ client/poof_game/Assets/Scripts/src/Building Information/ResourceBuildingInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceBuildingInformation : BuildingInformation{
+    public int FireGenerationRate { get; set; }
+    public int EarthGenerationRate { get; set; }
+    public int WaterGenerationRate { get; set; }
+    public int AirGenerationRate { get; set; }
+}
