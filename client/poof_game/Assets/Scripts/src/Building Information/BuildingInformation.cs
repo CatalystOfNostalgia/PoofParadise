39606acd62751dbbb2179d5432666fac274bb794
Ted <tkk21@case.edu>
2015-12-06 22:16:20 -0500

    building information made
    that handles id and building cost

---
+++ client/poof_game/Assets/Scripts/src/Building Information/BuildingInformation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// Object holding information about the base building
+/// </summary>
+public class BuildingInformation{
+    public int ID { get; set; }
+    public int FireCost { get; set; }
+    public int EarthCost { get; set; }
+    public int WaterCost { get; set; }
+    public int AirCost { get; set; }
+}
---
+++ client/poof_game/Assets/Scripts/src/Managers/BuildingInformationManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// A hardcoded class of the building information
+/// which includes the id, cost of buildings
+/// </summary>
+public class BuildingInformationManager : MonoBehaviour {
+    public static BuildingInformationManager buildingInformationManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
