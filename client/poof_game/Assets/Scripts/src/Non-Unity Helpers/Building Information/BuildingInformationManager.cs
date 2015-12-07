32abe014946d048dd264191c32b9b5ac8d4aa454
Ted <tkk21@case.edu>
2015-12-07 00:41:45 -0500

    reflect the level requirement change in create

--- client/poof_game/Assets/Scripts/src/Non-Unity Helpers/Building Information/BuildingInformationManager.cs
+++ client/poof_game/Assets/Scripts/src/Non-Unity Helpers/Building Information/BuildingInformationManager.cs
@@ -49,17 +49,17 @@ public class BuildingInformationManager {
 
     private void addResourceBuildingInfo()
     {
-        resourceBuildingInformationDict.Add("Fire Tree Lvl 1", new ResourceBuildingInformation(1, 50, 0, 0, 0, 5, 0, 0, 0));
-        resourceBuildingInformationDict.Add("Fire Tree Lvl 2", new ResourceBuildingInformation(2, 100, 0, 0, 0, 10, 0, 0, 0));
+        resourceBuildingInformationDict.Add("Fire Tree Lvl 1", new ResourceBuildingInformation(1, 50, 0, 0, 0, 1, 5, 0, 0, 0));
+        resourceBuildingInformationDict.Add("Fire Tree Lvl 2", new ResourceBuildingInformation(2, 100, 0, 0, 0, 2, 10, 0, 0, 0));
 
-        resourceBuildingInformationDict.Add("Pond Lvl 1", new ResourceBuildingInformation(3, 0, 0, 50, 0, 0, 0, 5, 0));
-        resourceBuildingInformationDict.Add("Pond Lvl 2", new ResourceBuildingInformation(4, 0, 0, 100, 0, 0, 0, 10, 0));
+        resourceBuildingInformationDict.Add("Pond Lvl 1", new ResourceBuildingInformation(3, 0, 0, 50, 0, 1, 0, 0, 5, 0));
+        resourceBuildingInformationDict.Add("Pond Lvl 2", new ResourceBuildingInformation(4, 0, 0, 100, 0, 2, 0, 0, 10, 0));
 
-        resourceBuildingInformationDict.Add("Windmill Lvl 1", new ResourceBuildingInformation(5, 0, 0, 0, 50, 0, 0, 0, 5));
-        resourceBuildingInformationDict.Add("Windmill Lvl 2", new ResourceBuildingInformation(6, 0, 0, 0, 100, 0, 0, 0, 10));
+        resourceBuildingInformationDict.Add("Windmill Lvl 1", new ResourceBuildingInformation(5, 0, 0, 0, 50, 1, 0, 0, 0, 5));
+        resourceBuildingInformationDict.Add("Windmill Lvl 2", new ResourceBuildingInformation(6, 0, 0, 0, 100, 2, 0, 0, 0, 10));
 
-        resourceBuildingInformationDict.Add("Cave Lvl 1", new ResourceBuildingInformation(7, 0, 50, 0, 0, 0, 5, 0, 0));
-        resourceBuildingInformationDict.Add("Cave Lvl 2", new ResourceBuildingInformation(8, 0, 100, 0, 0, 0, 10, 0, 0));
+        resourceBuildingInformationDict.Add("Cave Lvl 1", new ResourceBuildingInformation(7, 0, 50, 0, 0, 1, 0, 5, 0, 0));
+        resourceBuildingInformationDict.Add("Cave Lvl 2", new ResourceBuildingInformation(8, 0, 100, 0, 0, 2, 0, 10, 0, 0));
     }
 
     private void addDecorativeBuildingInfo()
