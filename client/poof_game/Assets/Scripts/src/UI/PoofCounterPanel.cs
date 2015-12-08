cd5acf751fd8fa54687a1038611286f17259e22c
jrg94 <jrg170@case.edu>
2015-12-08 18:45:06 -0500

    Cleaned up poof counter panel code

--- client/poof_game/Assets/Scripts/src/UI/PoofCounterPanel.cs
+++ client/poof_game/Assets/Scripts/src/UI/PoofCounterPanel.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
 
-
+/**
+ * TODO: Add a description
+ */
 public class PoofCounterPanel : GamePanel {
 
     public static PoofCounterPanel poofCounterPanel;
     private Text poofCounterText;
 
+    /**
+     * Overrides the basic start functionality
+     * for a game panel
+     */
 	override public void Start()
 	{
         poofCounterText = GetComponentInChildren<Transform>().GetComponentInChildren<Image>().GetComponentInChildren<Text>();
 		GeneratePanel();
 	}
 	
+    /**
+     * Resets text on this panel
+     */
 	override public void GeneratePanel(){
         poofCounterText.text = string.Format("{0}/{1}", SaveState.state.poofCount, SaveState.state.poofLimit);
 	}
-    
-    void Update()
-    {
-        // DO NOT DO THIS -> Basically freezes the game
-        // Instead: Find out who updates poofcount anc call Generate Panel from there
-        // GeneratePanel();
-    }
 }
