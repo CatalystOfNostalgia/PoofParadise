33ee8c29930a32a05f1799f2f9496fa71fc13e96
Ted <tkk21@case.edu>
2015-12-07 23:56:49 -0500

    poof manager manages poof gen rate

--- client/poof_game/Assets/Scripts/src/Managers/PoofManager.cs
+++ client/poof_game/Assets/Scripts/src/Managers/PoofManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PoofManager : MonoBehaviour {
 
+    public int poofGenerationRate { get; set; }
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,9 @@ public class PoofManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    //periodically roll a dice based on poof generation rate to spawn a poof
+        // could base it off of how many poofs exist vs. how many poofs we alreayd have
+        // like Delta poof * poofRate?
+        
 	}
 }
