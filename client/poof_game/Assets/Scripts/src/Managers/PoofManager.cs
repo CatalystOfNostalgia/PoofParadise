135f8021155a32a6a263d91d2c9505fab0066c72
Ted <tkk21@case.edu>
2015-12-08 00:26:46 -0500

    log to check poof rate

--- client/poof_game/Assets/Scripts/src/Managers/PoofManager.cs
+++ client/poof_game/Assets/Scripts/src/Managers/PoofManager.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class PoofManager : MonoBehaviour {
+
     public static PoofManager poofManager;
     public int poofGenerationRate { get; set; }
+
 	// Use this for initialization
 	void Start () {
         if (poofManager == null)
@@ -19,9 +21,9 @@ public class PoofManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    //periodically roll a dice based on poof generation rate to spawn a poof
+        //periodically roll a dice based on poof generation rate to spawn a poof
         // could base it off of how many poofs exist vs. how many poofs we alreayd have
         // like Delta poof * poofRate?
-        
+        Debug.Log(string.Format("[PoofManager] I can generate {0} per time", poofGenerationRate));
 	}
 }
