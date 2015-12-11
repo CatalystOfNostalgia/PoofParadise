9f0ba3cae8641b558a4e886e1adbac0dc1164473
Ted <tkk21@case.edu>
2015-12-11 13:55:46 -0500

    made touch sound script

---
+++ client/poof_game/Assets/Scripts/src/ObjectBehaviors/TouchSound.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    /**
+ * A method that works with
+ * the collider of this object
+ */
+    void OnMouseDown()
+    {
+        SoundManager.soundManager.playSoundEffect("poof1");
+    }
+
+    /**
+     * A method that works with
+     * the collider of this object
+     */
+    void OnMouseEnter()
+    {
+    }
+
+    /**
+     * A method that works with
+     * the collider of this object
+     */
+    private void OnMouseExit()
+    {
+
+    }
+}
